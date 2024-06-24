1781532843cc79314e6e6d687432d1c6497853e4
shakedadler <shaked.adler@gmail.com>
2024-06-24 20:06:53 +0300

    profile dog

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import DogProfiles from './DogProfiles';
+import MyProfile from './MyProfile';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/my-profile">
+            <MyProfile />
+          </Route>
+          <Route path="/dog-profile">
+            <DogProfiles />
+          </Route>
+          <Route path="/">
+            <div>
+              <h1>Welcome to the Dog Profiles</h1>
+              <ul>
+                <li>
+                  <Link to="/dog-profile">View Dog Profile</Link>
+                </li>
+                <li>
+                  <Link to="/my-profile">View My Profile</Link>
+                </li>
+              </ul>
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

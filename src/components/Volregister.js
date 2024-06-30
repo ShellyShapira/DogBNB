f84ebc69c7a882785145b37b3de9df47057ada95
Shelly Shapira <shelly.shapira@mail.huji.ac.il>
2024-06-30 20:51:39 +0300

    Your commit message

--- src/components/Volregister.js
+++ src/components/Volregister.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from '../styles/RegisterReserved.module.css';
 import Volform from './Volform';
+import ImageSection from './ImageSection';
+
 
 
 function RegisterVolunteer() {
   return (
     <div className={styles.container}>
+      <ImageSection />
       <Volform />
     </div>
   );

478c3ebb8052915d2506e13d24195f45f94560e2
Shelly Shapira <shelly.shapira@mail.huji.ac.il>
2024-07-04 20:54:45 +0300

    some changes

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -75,7 +75,7 @@ const Feed = () => {
                 image: profile.profilePic || 'images/default_dog.jpg', // Use a default image if not provided
                 name: profile.dogName || 'Your Dog',
                 city: profile.address || 'Your City',
-                description: profile.dogDetails || 'Your Description', // Ensure this matches your profile field
+                description: profile.dogDetails || 'a cute and loving dog', // Ensure this matches your profile field
                 startDate: reformatDate(e.target.elements.startDate.value),
                 endDate: reformatDate(e.target.elements.endDate.value),
             };
